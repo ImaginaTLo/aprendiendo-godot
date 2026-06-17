b1b1c2f2a684979a016d555de3c1f9f2ba2f72ac
ImaginaTLo <teo.gev.011@gmail.com>
2026-06-17 22:40:56 +0200

    fix: limite escenario

--- jugador.gd
+++ jugador.gd
@@ -5,3 +5,6 @@ var velocidad: float = 400.0
 func _process(delta: float) -> void:
 	var direccion: Vector2 = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	position += direccion * velocidad * delta
+	var limites_pantalla = get_viewport_rect().size
+	position.x = clamp(position.x, 0, limites_pantalla.x)
+	position.y = clamp(position.y, 0, limites_pantalla.y)
