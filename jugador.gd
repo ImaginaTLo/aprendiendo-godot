087656a31dcf9b6d5304f4278df309c8f42aa5b3
ImaginaTLo <teo.gev.011@gmail.com>
2026-06-22 20:32:36 +0200

    feat: mecánica sprint (if / else)

--- jugador.gd
+++ jugador.gd
@@ -1,10 +1,17 @@
 extends Sprite2D
 
-var velocidad: float = 400.0
+var velocidad_caminar: float = 400.0
+var velocidad_sprint: float = 800.0
+
+var velocidad_actual: float = 400.0
 
 func _process(delta: float) -> void:
+	if Input.is_action_pressed ("sprint"):
+		velocidad_actual = velocidad_sprint
+	else:
+		velocidad_actual = velocidad_caminar
 	var direccion: Vector2 = Input.get_vector("mover_izq", "mover_der", "mover_arriba", "mover_abajo")
-	position += direccion * velocidad * delta
+	position += direccion * velocidad_actual * delta
 	var limites_pantalla = get_viewport_rect().size
 	position.x = clamp(position.x, 0, limites_pantalla.x)
 	position.y = clamp(position.y, 0, limites_pantalla.y)
