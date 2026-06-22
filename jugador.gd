05b68b8a4c9aa7723cb9e0cc8878db0ec72acb7e
ImaginaTLo <teo.gev.011@gmail.com>
2026-06-22 18:27:43 +0200

    feat: asociando los moviminetos a las teclas: WASD

--- jugador.gd
+++ jugador.gd
@@ -3,7 +3,7 @@ extends Sprite2D
 var velocidad: float = 400.0
 
 func _process(delta: float) -> void:
-	var direccion: Vector2 = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var direccion: Vector2 = Input.get_vector("mover_izq", "mover_der", "mover_arriba", "mover_abajo")
 	position += direccion * velocidad * delta
 	var limites_pantalla = get_viewport_rect().size
 	position.x = clamp(position.x, 0, limites_pantalla.x)
