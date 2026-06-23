ab6f12a5d5f7664ed098bc4a1a77e5c4376d5b5c
ImaginaTLo <teo.gev.011@gmail.com>
2026-06-23 21:03:52 +0200

    feat: implementacion de HUD, variable int y casteo str para puntuacion

--- jugador.gd
+++ jugador.gd
@@ -2,8 +2,8 @@ extends Sprite2D
 
 var velocidad_caminar: float = 400.0
 var velocidad_sprint: float = 800.0
-
 var velocidad_actual: float = 400.0
+var puntuacion: int = 0
 
 func _process(delta: float) -> void:
 	if Input.is_action_pressed ("sprint"):
@@ -15,3 +15,10 @@ func _process(delta: float) -> void:
 	var limites_pantalla = get_viewport_rect().size
 	position.x = clamp(position.x, 0, limites_pantalla.x)
 	position.y = clamp(position.y, 0, limites_pantalla.y)
+
+
+func _on_detector_area_entered(area: Area2D) -> void:
+	area.queue_free()
+	puntuacion += 1
+	var marcador_ui = get_node("../Marcador")
+	marcador_ui.text = "Puntos: " + str(puntuacion)
