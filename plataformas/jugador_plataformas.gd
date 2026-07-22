6b70504fd31613313e75409656dec2e287bf50c4
ImaginaTLo <teo.gev.011@gmail.com>
2026-07-22 12:50:29 +0200

    feat: integracion de animaciones del slime y logica de estados

--- plataformas/jugador_plataformas.gd
+++ plataformas/jugador_plataformas.gd
@@ -1,5 +1,6 @@
 extends CharacterBody2D
 
+@onready var anim = $AnimatedSprite2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
@@ -20,5 +21,23 @@ func _physics_process(delta: float) -> void:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+		
+	# DIRECCIÓN Y ANIMACIÓN
+	if direction != 0:
+	# Si nos movemos, reproducir correr
+		anim.play("run")
+	# Girar el sprite dependiendo de si vamos a izquierda o derecha
+		if direction < 0:
+			anim.flip_h = true
+		else:
+			anim.flip_h = false
+	else:
+	# Si estamos quietos, reproducir idle
+		anim.play("idle")
+	
+# EL SALTO TIENE PRIORIDAD
+	if not is_on_floor():
+	# Si no estamos en el suelo, reproducir salto (sobrescribe lo anterior)
+		anim.play("jump")
 
 	move_and_slide()
